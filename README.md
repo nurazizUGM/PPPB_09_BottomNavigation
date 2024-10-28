aa653ccf556e9bf3c9963191dd8ed5dfd80be5f2
Nur Aziz <sukabaru09@gmail.com>
2024-10-29 00:11:18 +0700

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pertemuan 9
+
+Bottom Navigation
+
+# APK
+
+[Download](https://github.com/nurazizUGM/PPPB_09_BottomNavigation/releases)
---
+++ app/src/main/java/com/example/tugas09/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.tugas09
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.tugas09.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        with(binding){
+            val navController = findNavController(R.id.nav_host_fragment)
+            bottomNavigation.setupWithNavController(navController)
+        }
+    }
+}
